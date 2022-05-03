3b1006af5410a75ff5fc008f128806166e45de59
DesigneR- <limjunyi03@gmail.com>
2022-05-03 22:22:39 +0800

    Added .java files to test codes in PSPM1819.md

---
+++ Past Year Theory Questions/Resources/Test Codes/PSPM1819.java
@@ -0,0 +1,25 @@
+class PSPM1819{}
+
+class Q5{
+
+    public static void main(String[] args){
+
+        int a = 10, b = 5;
+        double c = 2, x = 4.2, y = 33;
+
+        c = a + b - x / c;
+        y = 56 % 5 / 2 + a;        
+        
+        System.out.println(c + " " + y);
+        
+        int n = 10;
+
+        while(n > 0){
+        
+            System.out.print(n + ", ");
+            n--;
+        }
+
+        System.out.print("Bravo!");
+    }
+}
